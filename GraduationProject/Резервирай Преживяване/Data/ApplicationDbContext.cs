d19627b118336fc4a08e91e040faac9fa4ab23e8
dimitardimitrov05 <109605825+dimitardimitrov05@users.noreply.github.com>
2024-01-14 13:39:12 +0200

    started project

---
+++ GraduationProject/Резервирай Преживяване/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Резервирай_Преживяване.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
